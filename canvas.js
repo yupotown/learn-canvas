4544704a8cfbdeaf460fd416bbb62b5d3c173a62
Yupo <yupotown@gmail.com>
2015-11-30 16:41:42 +0900

    キー入力イベントとマウス移動イベントも追加

--- canvas.js
+++ canvas.js
@@ -2,6 +2,10 @@
 var context; // context
 var width = 640, height = 480;
 var x, y, w, h;
+var dx, dy;
+
+// マウスのクライアント座標
+var mouseX, mouseY;
 
 function run() {
 	var canvas = document.getElementById('main-canvas');
@@ -12,9 +16,13 @@ function run() {
 		y = 100;
 		w = 32;
 		h = 32;
+		dx = 2;
+		dy = 0;
 
 		// 入力イベント
 		canvas.addEventListener('click', click, true);
+		canvas.addEventListener('mousemove', move, true);
+		window.addEventListener('keydown', keydown, true);
 
 		// 33[ms]ごとに render を実行
 		setInterval(render, 33);
@@ -29,7 +37,11 @@ function render() {
 	context.fillStyle = 'rgb(255, 0, 0)';
 	context.fillRect(x, y, w, h);
 
-	x += 2;
+	context.fillStyle = 'rgb(255, 255, 0)';
+	context.fillRect(mouseX - 2, mouseY - 2, 4, 4);
+
+	x += dx;
+	y += dy;
 }
 
 // クリック時の処理
@@ -37,4 +49,25 @@ function click(event) {
 	var rect = event.target.getBoundingClientRect();
 	x = event.clientX - rect.left - w / 2;
 	y = event.clientY - rect.top - h / 2;
+}
+
+// マウス移動時の処理
+function move(event) {
+	var rect = event.target.getBoundingClientRect();
+	mouseX = event.clientX - rect.left;
+	mouseY = event.clientY - rect.top;
+}
+
+// キー押下時の処理
+function keydown(event) {
+	var code = event.keyCode;
+
+	switch (code) {
+	case 37: // left
+		dx -= 1;
+		break;
+	case 39: // right
+		dx += 1;
+		break;
+	}
 }
