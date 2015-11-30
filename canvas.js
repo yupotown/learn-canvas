aee1f65dc871dc381455883302fb9102d43ebacb
Yupo <yupotown@gmail.com>
2015-11-30 16:01:38 +0900

    アニメーションを追加

--- canvas.js
+++ canvas.js
@@ -1,11 +1,28 @@
 
+var ctx; // context
+var width = 640, height = 480;
+var x, y;
+
 function run() {
 	var canvas = document.getElementById('main-canvas');
 	if (canvas.getContext) {
-		var context = canvas.getContext('2d');
+		ctx = canvas.getContext('2d');
 
-		context.strokeStyle = 'rgb(255, 128, 128)';
-		context.fillStyle = 'rgb(255, 0, 0)';
-		context.fillRect(100, 100, 200, 200);
+		x = 100;
+		y = 100;
+
+		// 33[ms]ごとに render を実行
+		setInterval(render, 33);
 	}
+}
+
+function render() {
+	ctx.fillStyle = 'rgb(0, 0, 0)';
+	ctx.fillRect(0, 0, width, height);
+
+	ctx.strokeStyle = 'rgb(255, 128, 128)';
+	ctx.fillStyle = 'rgb(255, 0, 0)';
+	ctx.fillRect(x, y, 32, 32);
+
+	x += 2;
 }
