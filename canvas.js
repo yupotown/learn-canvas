a316040d01104758c203586d0379694a951e84b5
Yupo <yupotown@gmail.com>
2015-11-30 16:14:33 +0900

    クリックイベントの処理を追加

--- canvas.js
+++ canvas.js
@@ -1,15 +1,20 @@
 
-var ctx; // context
+var context; // context
 var width = 640, height = 480;
-var x, y;
+var x, y, w, h;
 
 function run() {
 	var canvas = document.getElementById('main-canvas');
 	if (canvas.getContext) {
-		ctx = canvas.getContext('2d');
+		context = canvas.getContext('2d');
 
 		x = 100;
 		y = 100;
+		w = 32;
+		h = 32;
+
+		// 入力イベント
+		canvas.addEventListener('click', click, true);
 
 		// 33[ms]ごとに render を実行
 		setInterval(render, 33);
@@ -17,12 +22,19 @@ function run() {
 }
 
 function render() {
-	ctx.fillStyle = 'rgb(0, 0, 0)';
-	ctx.fillRect(0, 0, width, height);
+	context.fillStyle = 'rgb(0, 0, 0)';
+	context.fillRect(0, 0, width, height);
 
-	ctx.strokeStyle = 'rgb(255, 128, 128)';
-	ctx.fillStyle = 'rgb(255, 0, 0)';
-	ctx.fillRect(x, y, 32, 32);
+	context.strokeStyle = 'rgb(255, 128, 128)';
+	context.fillStyle = 'rgb(255, 0, 0)';
+	context.fillRect(x, y, w, h);
 
 	x += 2;
+}
+
+// クリック時の処理
+function click(event) {
+	var rect = event.target.getBoundingClientRect();
+	x = event.clientX - rect.left - w / 2;
+	y = event.clientY - rect.top - h / 2;
 }
